269ce9befd6816f6409696791665990d5d67c830
arkar <arkar20011@gmail.com>
2022-11-21 12:30:02 +0630

    get req auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/user/currentuser", (req, res) => {
+  return res.send("Current User");
+});
+
 app.listen(3000, () => {
   console.log("running in port 3000!");
 });
