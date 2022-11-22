02d045ad92ab0f2c9f8c91f0453415580a3f79fc
arkar <arkar20011@gmail.com>
2022-11-23 01:16:09 +0630

    signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.get("/api/users/signup", (req, res) => {
-  return res.send("sign up route");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw Error("something went wrong");
+    }
+    console.log("Creating a user...");
+    throw Error("something went wrong");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
