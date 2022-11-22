7f3d3246233c305b19bd89837004d68f84f7aabc
arkar <arkar20011@gmail.com>
2022-11-23 01:14:14 +0630

    separate routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import { json } from "body-parser";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 const app = express();
+app.use(json());
 
-app.use(express.json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.get("/api/users/currentuser", (req, res) => {
-  return res.send("Current User");
-});
+app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("running in port 3000!");
+  console.log("Listening on port 3000!!");
 });
