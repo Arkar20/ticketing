292e0d1749f6dce2ed7b9aaf8171b7041f457185
arkar <arkar20011@gmail.com>
2022-12-09 17:37:59 +0630

    current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from 'express';
-
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import "cookie-session";
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.json({ currentuser: null });
+  }
+
+  try {
+    var decoded = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
+
+    return res.json({ currentuser: decoded });
+  } catch (err) {
+    return res.json({ currentuser: null });
+  }
 });
 
 export { router as currentUserRouter };
