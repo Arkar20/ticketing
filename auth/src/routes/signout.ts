a328190746f62160e0eb6803888b1d7acb593fd2
arkar <arkar20011@gmail.com>
2022-12-09 17:39:27 +0630

    signout api

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
+router.post("/api/users/signout", (req, res) => {
+  req.session = null;
+  return res.json({});
 });
 
 export { router as signoutRouter };
