3f620f097ad40a4b3b4eb2af476dc0f048f9f63e
arkar <arkar20011@gmail.com>
2022-12-09 13:38:38 +0630

    cookie session setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,17 @@ import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 const app = express();
+app.set("trust proxy", true);
 
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true, //allowing only https
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
