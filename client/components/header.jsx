2c9104299aff80fa56134724f37d4b98f87b49df
arkar <arkar20011@gmail.com>
2022-12-14 18:08:17 +0630

    header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-
-export function Header() {
+import Link from "next/link";
+export function Header({ currentUser }) {
+  const links = [
+    !currentUser && { label: "Sign In", href: "/signin" },
+    !currentUser && { label: "Sign Up", href: "/signup" },
+    currentUser && { label: "Sign Out", href: "/signout" },
+  ]
+    .filter((link) => link)
+    .map((link, index) => (
+      <li style={{ padding: "10px" }}>
+        <Link key={index} href={link.href}>
+          {link.label}
+        </Link>
+      </li>
+    ));
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <h1>Logo</h1>
-      <ul style={{ display: "flex", listStyle: "none" }}>
-        <li style={{ padding: "10px" }}>Sign In</li>
-        <li>Sign Out</li>
-        <li>Sign Up</li>
-      </ul>
+      <ul style={{ display: "flex", listStyle: "none" }}>{links}</ul>
     </div>
   );
 }
--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Header } from "../components";
+import { axiosNext } from "../service";
 export default function App({ Component, pageProps }) {
+  const { currentUser } = pageProps;
   return (
     <>
-      <Header />
+      <Header currentUser={currentUser} />
       <Component {...pageProps}></Component>;
     </>
   );
 }
+App.getInitialProps = async (context) => {
+  if (context.Component.getInitialProps) {
+    context.Component.getInitialProps(context.ctx);
+  }
+  const response = await axiosNext(context.ctx).get("/api/users/currentuser");
+
+  return { pageProps: { ...response.data } };
+};
