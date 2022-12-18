d008a1ff09066693222fe1a590a2bc3ee0707be8
arkar <arkar20011@gmail.com>
2022-12-18 21:17:03 +0630

    updatr auth depl for mongo env url

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,13 +2,16 @@ import { BadRequest } from "@jeffery_microservice/common";
 import mongoose from "mongoose";
 import app from "./app";
 
-if (!process.env.JWT_SECRET) {
-  throw new BadRequest("JWT Secret Key Not Exists");
-}
 async function start() {
+  if (!process.env.JWT_SECRET) {
+    throw new BadRequest("JWT Secret Key Not Exists");
+  }
+  if (!process.env.MONGO_URL) {
+    throw Error("Mongo Env needs to be defined");
+  }
   try {
     mongoose.set("strictQuery", true);
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("mongo Connected");
   } catch (error) {
     console.log("Error Connecting to MongoDB");
