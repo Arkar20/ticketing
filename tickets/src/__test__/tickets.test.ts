84054f2c28d3c7a8c5ccb3fa040e8b508e7690d6
arkar <arkar20011@gmail.com>
2022-12-20 13:22:04 +0630

    tdd create ticket route

--- tickets/src/__test__/tickets.test.ts
+++ tickets/src/__test__/tickets.test.ts
@@ -12,26 +12,18 @@ it("requires to login first", async () => {
 
   expect(response.status).toEqual(401);
 });
-it("only login user can access the route", () => {
-  request
-    .agent(app)
-    .post("/api/tickets")
-    .set("Cookie", global.signin())
-    .send()
-    .end((err, response) => {
-      expect(response.status).toEqual(200);
-    });
-});
+
 it("can create ticket", async () => {
   const tickets = await Ticket.find({});
+
   expect(tickets.length).toEqual(0);
 
-  request
+  await request
     .agent(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
-    .send({ title: "test", desc: "test desc" })
-    .end((err, res) => {
-      expect(tickets.length).toEqual(1);
-    });
+    .send({ title: "test", desc: "test desc" });
+
+  const ticketsAfterCreate = await Ticket.find({});
+  expect(ticketsAfterCreate.length).toEqual(1);
 });
--- tickets/src/routes/tickets.ts
+++ tickets/src/routes/tickets.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
 import { auth } from "@jeffery_microservice/common";
-
+import { Ticket } from "../model";
 const ticketRouter = express.Router();
 
-ticketRouter.post("/tickets", auth, (req: Request, res: Response) => {
-  return res.send(req.currentUser);
+ticketRouter.post("/tickets", auth, async (req: Request, res: Response) => {
+  const { title, desc } = req.body;
+
+  const user_id = req.currentUser!.id;
+
+  const ticket = await Ticket.build({ title, desc, user_id }).save();
+
+  return res.status(201).send(ticket);
 });
 
 export default ticketRouter;
