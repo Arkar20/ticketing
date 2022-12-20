90b23703e4059f67fe7fd9cb79168a83f4eb999e
arkar <arkar20011@gmail.com>
2022-12-20 20:58:39 +0630

    queue group

--- nats-streaming/src/listener.ts
+++ nats-streaming/src/listener.ts
@@ -1,14 +1,17 @@
 import nats, { Message } from "node-nats-streaming";
-
+import { randomBytes } from "crypto";
 console.clear();
-const stan = nats.connect("ticketing", "abc", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("listener is listening");
 
-  const subscribe = stan.subscribe("ticket:created");
+  const subscribe = stan.subscribe(
+    "ticket:created",
+    "ticket-created-service-group"
+  );
 
   subscribe.on("message", (msg: Message) => {
     console.log(msg.getData());
