88284705b6d738d6c89e3950cc21cbb9ef70f4b4
arkar <arkar20011@gmail.com>
2022-12-28 20:55:40 +0630

    delete route for order

---
+++ order/src/__test__/delete.test.ts
@@ -0,0 +1,59 @@
+import request from "supertest";
+import app from "../app";
+import { Ticket } from "../model";
+import mongoose from "mongoose";
+import { OrderStatus } from "@jeffery_microservice/common";
+it("has a delete order route", async () => {
+  const res = await request(app).delete("/api/orders/1");
+  expect(res.status).not.toEqual(404);
+});
+
+it("need to pass valid mongoose id", async () => {
+  const resOne = await request(app)
+    .delete("/api/orders/1")
+    .set("Cookie", global.signin())
+    .send();
+
+  expect(resOne.status).toEqual(400);
+
+  const res = await request(app)
+    .delete(`/api/orders/${new mongoose.Types.ObjectId()}`)
+    .set("Cookie", global.signin())
+    .send();
+
+  expect(res.status).toEqual(400);
+});
+
+it("need to login to delete the single order", async () => {
+  const res = await request(app)
+    .get(`/api/orders/${new mongoose.Types.ObjectId()}`)
+    .send();
+
+  expect(res.status).toEqual(401);
+});
+
+it("auth user needs to own the order to delete", async () => {
+  const ticket = await Ticket.build({
+    desc: "test desc",
+    title: "hello title",
+    price: 100,
+  }).save();
+
+  const userId = global.signin();
+
+  const orderRes = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userId)
+    .send({ ticket_id: ticket.id });
+
+  const res = await request(app)
+    .delete(`/api/orders/${orderRes.body.id}`)
+    .set("Cookie", userId)
+    .send();
+
+  expect(res.status).toEqual(201);
+  expect(res.body.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo("needs to emit order create event");
+it.todo("needs to emit order canceled event");
--- order/src/routes/delete.ts
+++ order/src/routes/delete.ts
@@ -1,23 +1,44 @@
 import express, { Request, Response } from "express";
 import {
   auth,
+  OrderStatus,
   Unauthorize,
   validationHandler,
 } from "@jeffery_microservice/common";
-import { Ticket } from "../model";
-import { body } from "express-validator";
+import { Order, Ticket } from "../model";
+import { param } from "express-validator";
 import { TicketUpdatedPublisher } from "../events/publisher/TicketUpdatedPublisher";
 import { natsWrapper as NatsWrapper } from "../nats-connect";
 import { BadRequest } from "@jeffery_microservice/common";
+import mongoose from "mongoose";
 const orderDeleteRoute = express.Router();
 
 orderDeleteRoute.delete(
   "/orders/:id",
   auth,
+  [
+    param("id")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Mal Form ID value"),
+  ],
   validationHandler,
 
   async (req: Request, res: Response) => {
-    return res.status(201).send("update order route");
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      throw new BadRequest("Order not found");
+    }
+
+    if (order.user_id !== req.currentUser!.id) {
+      throw new Unauthorize("You are authorized to cancel.");
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    return res.status(201).send(order);
   }
 );
 
